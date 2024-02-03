dd8fceb34973a56b626985c5ca55864d73b79699
Sheraz Howlader <mdsherazhowlader@gmail.com>
2024-02-03 15:44:17 +0600

    fix ip problem in server

--- app/Http/Controllers/GeoLocationController.php
+++ app/Http/Controllers/GeoLocationController.php
@@ -7,13 +7,14 @@ class GeoLocationController extends Controller
     public function location()
     {
         $ip             = file_get_contents("http://ipecho.net/plain");
-        $ip_data_json   = file_get_contents("http://www.geoplugin.net/json.gp?ip=$ip");
+        $ip_address     =  config('app.debug') ? $ip : $_SERVER['REMOTE_ADDR'];
+        $ip_data_json   = file_get_contents("http://www.geoplugin.net/json.gp?ip=$ip_address");
         $ip_data        = json_decode($ip_data_json);
 
         if ($ip_data !== null) {
             unset($ip_data->geoplugin_credit);
             return response()->json([
-                'ip'                => $ip,
+                'ip'                => $ip_address,
                 'continent_name'    => $ip_data->geoplugin_continentName,
                 'country_name'      => $ip_data->geoplugin_countryName,
                 'capital'       => $ip_data->geoplugin_city,

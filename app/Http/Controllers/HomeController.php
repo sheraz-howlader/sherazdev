ebe6e50abbaa9e806ec16b0371176af42cad3386
Sheraz H. <mdsherazhowlader@gmail.com>
2026-01-21 15:30:54 +0600

    conf:update docker info

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,27 +15,12 @@ class HomeController extends Controller
 {
     public function __invoke()
     {
-        // $ip_data_json   = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $_SERVER['REMOTE_ADDR']));
-
-        // Visitor::updateOrCreate([
-        //     'user_agent'=> request()->userAgent(),
-        //     'ip'        => $_SERVER['REMOTE_ADDR'],
-        // ],[
-        //     'continent_name'   => $ip_data_json->geoplugin_continentName ?? '',
-        //     'country'   => $ip_data_json->geoplugin_countryName ?? '',
-        //     'capital'   => $ip_data_json->geoplugin_city ?? '',
-        //     'city'      => $ip_data_json->geoplugin_regionName ?? '',
-        //     'timezone'  => $ip_data_json->geoplugin_timezone ?? '',
-        //     'latitude'  => $ip_data_json->geoplugin_latitude ?? '',
-        //     'longitude' => $ip_data_json->geoplugin_longitude ?? '',
-        // ]);
-
         $user       = User::first();
         $about_me   = AboutMe::first();
         $services   = Service::all();
         $educations  = Education::where('status', true)->get();
         $experiences = Experience::where('status', true)->get();
-        $portfolios = Portfolio::all();
+        $portfolios  = Portfolio::all();
 
         return view('frontend.layouts.app', compact('user', 'about_me', 'services','educations','experiences','portfolios'));
     }
